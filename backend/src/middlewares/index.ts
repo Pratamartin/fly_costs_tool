e9adc409c8f5396139b5609c24c9fd40a52f345d
letabilis <192747580+letabilis@users.noreply.github.com>
2026-04-11 22:15:22 -0400

    feat(auth): add requireRole middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as cors } from './cors'
 export { default as logger } from './logger'
 export { default as requireAuth } from './require-auth'
+export { default as requireRole } from './require-role'
