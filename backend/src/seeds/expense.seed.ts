5ecd3acdc2f24b8596494047ec554aab7d22ab09
letabilis <192747580+letabilis@users.noreply.github.com>
2026-04-25 13:43:51 -0400

    seed: update script

--- backend/src/seeds/expense.seed.ts
+++ backend/src/seeds/expense.seed.ts
@@ -1,42 +1,33 @@
 /* eslint-disable no-console */
-import type { ExpenseRequest } from '@/generated/prisma/client'
-import { Decimal } from '@prisma/client/runtime/client'
-import { ExpenseRequestStatus, ExpenseTopic } from '@/generated/prisma/client'
+import type { Prisma } from '@/generated/prisma/client'
+import { ExpenseRequestStatus } from '@/generated/prisma/client'
 import prisma from '@/lib/orm'
-import { ID_ALUNO, ID_PROJ_IA, ID_PROJ_ROBOTICA } from '../constants/seed.constant'
+import { ID_ALUNO, ID_PROJ_IA } from '../constants/seed.constant'
 
-type DummyExpense = Omit<ExpenseRequest, 'createdAt' | 'updatedAt'>
-
-const dummyExpenses: DummyExpense[] = [
+export const dummyExpenses: Prisma.ExpenseRequestCreateInput[] = [
   {
     id: 'd290f1ee-6c54-4b01-90e6-d701748f0851',
     title: 'Inscrição SBSC 2026',
     description: 'Apresentação de artigo sobre braços mecânicos.',
-    amount: new Decimal(450.00),
-    topic: ExpenseTopic.INSCRICAO,
     status: ExpenseRequestStatus.APROVADO,
-    studentId: ID_ALUNO,
-    projectId: ID_PROJ_IA,
+    student: { connect: { id: ID_ALUNO } },
+    project: { connect: { id: ID_PROJ_IA } },
   },
   {
-    id: 'e310a2ff-7d65-5c12-01f7-e812859g1962',
+    id: 'ef9ac2fc-a3a2-488b-b06b-480e57315c4f',
     title: 'Passagens para Manaus',
     description: 'Voo para participação no simpósio de robótica.',
-    amount: Decimal(1200.50),
-    topic: ExpenseTopic.PASSAGEM,
     status: ExpenseRequestStatus.REJEITADO,
-    studentId: ID_ALUNO,
-    projectId: ID_PROJ_IA,
+    student: { connect: { id: ID_ALUNO } },
+    project: undefined,
   },
   {
-    id: 'f421b3aa-8e76-6d23-12a8-f923960h2073',
+    id: '9e730bb7-6123-4363-8f87-e37f907a3246',
     title: 'Alimentação - Diárias',
     description: 'Refeições durante os 3 dias de evento.',
-    amount: Decimal(679.00),
-    topic: ExpenseTopic.HOSPEDAGEM,
     status: ExpenseRequestStatus.PENDENTE,
-    studentId: ID_ALUNO,
-    projectId: ID_PROJ_ROBOTICA,
+    student: { connect: { id: ID_ALUNO } },
+    project: undefined,
   },
 ]
 
--- backend/src/seeds/project.seed.ts
+++ backend/src/seeds/project.seed.ts
@@ -1,32 +1,27 @@
 /* eslint-disable no-console */
-import type { Project } from '@/generated/prisma/client'
+import type { Prisma } from '@/generated/prisma/client'
 import { Decimal } from '@prisma/client/runtime/client'
-import { ExpenseTopic } from '@/generated/prisma/client'
 import prisma from '@/lib/orm'
 import { ID_PROJ_IA, ID_PROJ_ROBOTICA } from '../constants/seed.constant'
+import { dummyExpenseCategories } from './expense.category.seed'
 
-type DummyProject = Omit<Project, 'createdAt' | 'updatedAt'>
-
-export const dummyProjects: DummyProject[] = [
+export const dummyProjects: Prisma.ProjectCreateInput[] = [
   {
     id: ID_PROJ_ROBOTICA,
     name: 'Laboratório de Robótica Avançada',
     code: 'ROBOTICA-26',
     budget: new Decimal(15000.00),
-    expenseTopics: [
-      ExpenseTopic.INSCRICAO,
-      ExpenseTopic.PASSAGEM,
-      ExpenseTopic.HOSPEDAGEM,
-    ],
+    expenseCategories: {
+      connect: dummyExpenseCategories
+        .map(expenseCategory => ({ id: expenseCategory.id })),
+    },
   },
   {
     id: ID_PROJ_IA,
     name: 'Pesquisa em IA Aplicada',
     code: 'IA-WEB-26',
     budget: new Decimal(25000.00),
-    expenseTopics: [
-      ExpenseTopic.PASSAGEM,
-    ],
+
   },
 ]
 
