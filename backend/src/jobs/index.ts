59b53535c9e31ad5e1fd80c0c1eff2187c45cf72
letabilis <192747580+letabilis@users.noreply.github.com>
2026-06-12 19:34:53 -0400

    infra: add db scheduled record cleanup  job

--- backend/src/jobs/index.ts
+++ backend/src/jobs/index.ts
@@ -1,7 +1,9 @@
 import { boss, JobManager } from '@/lib/jobs'
+import { CleanupJob } from './cleanup.job'
 import { GenerateReportJob } from './generate-report.job'
 import { SendEmailJob } from './send-email.job'
 
 export const jobManager = new JobManager(boss)
   .register(new SendEmailJob(boss))
   .register(new GenerateReportJob(boss))
+  .register(new CleanupJob(boss))
